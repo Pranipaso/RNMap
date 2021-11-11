467b393941bfdfc38f39b8773afa709c4c3a094d
Darshana <darshana@plusinfosys.com>
2021-11-11 11:41:56 +0530

    git lfs migration

---
+++ lib/typescript/index.d.ts
@@ -0,0 +1,7 @@
+import { ViewStyle } from 'react-native';
+declare type PlusinfosysProps = {
+    region: object;
+    style: ViewStyle;
+};
+export declare const Pimap: import("react-native").HostComponent<PlusinfosysProps>;
+export default Pimap;
